d8b0ab1ce71343c0a2fdf05063e54825bbec3551
vipz3 <vipz3@mail.ru>
2018-09-20 01:09:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import AppNavbar from "./components/layout/AppNavbar";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <h1>Hello</h1>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
